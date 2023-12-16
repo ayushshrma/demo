bf787250fe14a2bdba12b5f40b8b77b9ed6f8a5e
ayush sharma <ayush.sharma2@tothenew.com>
2023-12-16 23:09:28 +0530

    changes for api

--- src/main/java/com/hospital/management/demo/service/JwtTokenService.java
+++ src/main/java/com/hospital/management/demo/service/JwtTokenService.java
@@ -6,9 +6,7 @@ import java.util.Date;
 public interface JwtTokenService {
     String generateToken(HospitalStaff user, Date expireDate);
     void saveAccessToken(String token, HospitalStaff user,Date expiresAt);
-    void saveRefreshToken(String token,HospitalStaff user,Date expiresAt);
     Boolean validateToken(String token);
     Boolean disableTokens(String token);
     void disableAccessTokens(HospitalStaff user);
-    String refreshTheAccessToken(String refreshToken);
 }
